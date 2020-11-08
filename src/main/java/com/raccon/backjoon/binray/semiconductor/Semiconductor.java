3b5f781e52045804509bc6a6fb8335267f8c8636
YoonjaeSong <thddbswo14@gmail.com>
2020-11-08 15:07:23 +0900

    백준-반도체설계(2352)

---
+++ src/main/java/com/raccon/backjoon/binray/semiconductor/Semiconductor.java
@@ -0,0 +1,46 @@
+package com.raccon.backjoon.binray.semiconductor;
+
+import java.util.ArrayList;
+import java.util.List;
+
+// 반도체 설계 (2352)
+public class Semiconductor {
+    public int solution(int[] ports){
+        int index = 0;
+        int[] memory = new int[ports.length];
+        List<Integer> list = new ArrayList<>();
+
+        // 초기 세팅
+        list.add(ports[0]);
+        memory[0] = 1;
+
+        for(int i=1; i<ports.length; i++){
+            if(list.get(list.size()-1) > ports[i]){
+                index = lowerBoundary(list, ports[i]);
+                list.set(index, ports[i]);
+                memory[i] = index;
+                continue;
+            }
+
+            list.add(ports[i]);
+            memory[i] = list.size();
+        }
+
+        return list.size();
+    }
+
+    private  int lowerBoundary(List<Integer> list, int target){
+        int start = 0, end = list.size() - 1;
+
+        while(start < end){
+            int mid = (start + end) / 2;
+            if(list.get(mid) >= target){
+                end = mid;
+            }else{
+                start = mid + 1;
+            }
+        }
+
+        return end;
+    }
+}
